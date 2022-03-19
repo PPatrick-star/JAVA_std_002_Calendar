927397255668ea52b6e5f3d31af2d7505baadeef
Tae Young Ryu <yty0819@gmail.com>
2022-03-19 23:56:00 +0900

    Close #4  - Get month date

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,16 +1,21 @@
+import java.util.Scanner;
+
 public class Calendar {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int month;
 
+        System.out.println("보고싶은 월을 입력하세요");
+        month = sc.nextInt();
+        if (1<= month && 12 >= month){
+            sc.close();
 
-
-
-        System.out.println("일  월  화 수 목 금  토");
-        System.out.println("--------------------");
-        System.out.println(" 1  2  3  4  5  6  7");
-        System.out.println(" 8  9 10 11 12 13 14");
-        System.out.println("15 16 17 18 19 20 21");
-        System.out.println("22 23 24 25 26 27 28");
+            int[] dateList = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            System.out.printf("%d 월의 일수는 %d 입니다", month, dateList[month - 1]);
+        } else {
+            System.out.println("다시 입력해주세요");
+        }
     }
 }
