23da8c2f3fe68c8ac8bad242fb5dd998e768ee8c
Tae Young Ryu <yty0819@gmail.com>
2022-03-20 00:03:34 +0900

    Close #4  - Use method

--- src/Calendar.java
+++ src/Calendar.java
@@ -5,6 +5,7 @@ public class Calendar {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        Calendar cal = new Calendar();
         int month;
 
         System.out.println("보고싶은 월을 입력하세요");
@@ -12,8 +13,7 @@ public class Calendar {
         if (1<= month && 12 >= month){
             sc.close();
 
-            int[] dateList = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            System.out.printf("%d 월의 일수는 %d 입니다", month, dateList[month - 1]);
+            System.out.printf("%d 월의 일수는 %d 입니다", month, cal.getMonthDate(month));
         } else {
             System.out.println("다시 입력해주세요");
         }
