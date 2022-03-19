302a4a5ff86b5bd606cd2dfe820264d43a2ad4a4
Tae Young Ryu <yty0819@gmail.com>
2022-03-20 00:20:39 +0900

    Close #6  - Update repeat

--- src/Calendar.java
+++ src/Calendar.java
@@ -6,17 +6,25 @@ public class Calendar {
 
         Scanner sc = new Scanner(System.in);
         Calendar cal = new Calendar();
-        int month;
+        int month, repeat;
 
-        System.out.println("보고싶은 월을 입력하세요");
-        month = sc.nextInt();
-        if (1<= month && 12 >= month){
-            sc.close();
+        System.out.println("반복 횟수 입력");
+        repeat = sc.nextInt();
 
-            System.out.printf("%d 월의 일수는 %d 입니다", month, cal.getMonthDate(month));
-        } else {
-            System.out.println("다시 입력해주세요");
+        for (int i = 0; i < repeat; i++) {
+
+            System.out.println("보고싶은 월을 입력하세요");
+            month = sc.nextInt();
+
+            if (1 <= month && 12 >= month) {
+
+                System.out.printf("%d 월의 일수는 %d 입니다", month, cal.getMonthDate(month));
+            } else {
+                System.out.println("다시 입력해주세요");
+            }
         }
+        sc.close();
+        System.out.println("프로그램 종료");
     }
 
     private final int[] MONTH_DAYS_LIST = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
